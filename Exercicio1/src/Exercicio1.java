ded05963c2acd6b6214bc798f23bae1a9966a6ee
JessycaCastro <jessycacastro@hotmail.com>
2022-03-15 10:34:24 -0300

    Exercicio1

---
+++ Exercicio1/src/Exercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		double nota1, nota2, media;
+		System.out.println("Por favor, digite sua primeira nota");
+		nota1 = teclado.nextDouble();
+		System.out.println("Agora, digite a segunda");
+		nota2 = teclado.nextDouble();
+		media = (nota1 + nota2) / 2;
+		System.out.println("Sua média foi: " + media);
+	}
+
+}
