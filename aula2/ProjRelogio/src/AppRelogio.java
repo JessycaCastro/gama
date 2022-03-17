046632f9f92c83bbc8ce8d0c852d16f15e89b7a0
JessycaCastro <jessycacastro@hotmail.com>
2022-03-17 10:18:38 -0300

    relogio

--- aula2/ProjRelogio/src/AppRelogio.java
+++ aula2/ProjRelogio/src/AppRelogio.java
@@ -1,7 +1,11 @@
 
 public class AppRelogio {
 	public static void main(String[] args) {
-		
+		Relogio relogio1 = new Relogio(2, 12, 30);
+
+		relogio1.setHora(4);
+		relogio1.setMinuto(90);
+		System.out.println(relogio1.getHoraAtual());
 	}
 
 }
